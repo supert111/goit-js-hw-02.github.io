3ca5607fa3b97be99409d47e2590de236d4e3171
supert111 <31413087+supert111@users.noreply.github.com>
2020-12-06 18:20:54 +0200

    Add files via upload

---
+++ js/task3.js
@@ -0,0 +1,21 @@
+const findLongestWord = function(string) {
+  if (string === Number) {
+      return;
+  }
+  let stringSplit = string.split(' ');
+  let longestWord = 0;
+  let word = null;
+  for (let i = 0; i < stringSplit.length; i++) {
+    if (stringSplit[i].length > longestWord) {
+      longestWord = stringSplit[i].length;
+      word = stringSplit[i];
+    }
+ }
+ return word;
+};
+
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
---
+++ js/task6.js
@@ -0,0 +1,23 @@
+let input;
+const numbers = [];
+let total = 0;
+
+
+do {
+    input = prompt('Введите число');
+
+    if (isNaN(input)) {
+        alert('Было введено не число, попробуйте еще раз');
+    }
+    else {
+        numbers.push(input);   
+    }
+} while (input !== null); 
+    
+        for (let number of numbers) {
+            number = Number(number);                
+            total += number; 
+        }   
+        console.log(numbers);
+
+alert(`Общая сумма чисел равна [${total}]`);
